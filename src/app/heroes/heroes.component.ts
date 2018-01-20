c6aec9ebc28a3ddd7f46d921f8f9daee81cdc189
User Name <pengfluf@tutanota.com>
2018-01-21 02:58:54 +0300

    Messages Service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'heroes',
+  selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
